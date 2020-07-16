b60dc95b5845d04854fe384736b34bfea936a690
ZHENGJIAN LIU <comezheng@gmail.com>
2020-07-16 23:03:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "";
+var topic = "Let\'s talk about .";
 
 var conversation = `${topic}`;
